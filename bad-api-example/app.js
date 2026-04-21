77fc81c72b61246f1806a264e09865d13f75870e
Sankar Srinivasan <petra.srini@gmail.com>
2026-04-21 16:53:34 +0530

    Create app.js

---
+++ bad-api-example/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/public", (req, res) => {
+  res.json({ message: "This is public" });
+});
+
+app.get("/admin", (req, res) => {
+  // No auth check at all
+  res.json({ message: "Welcome admin" });
+});
+
+app.post("/transfer", (req, res) => {
+  const { amount } = req.body;
+
+  // No validation
+  res.json({ success: true, amount });
+});
+
+app.listen(3000, () => console.log("Bad API running on 3000"));
